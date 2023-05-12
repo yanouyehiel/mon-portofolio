54ef60ef4bfb974ab5f64c659dfc484465e9ae19
Yanou Yehiel <85465004+yanouyehiel@users.noreply.github.com>
2023-05-12 21:33:47 +0100

    Delete includes directory

--- includes/footer.php
+++
@@ -1,6 +0,0 @@
-<div class="copyright py-sm-5 py-4 text-center">
-  <div class="container">
-    <p class="copy-footer-29">© 2022 Mon, Portfolio. All rights reserved | Design by <a
-        href="#">Yehiel Yanou</a></p>
-  </div>
-</div>
